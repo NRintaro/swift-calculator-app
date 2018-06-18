de9302bffc74a7a67173868fa40fd34364ddcd1c
r-nakamura-oliva <rintaro.nakamura@oliva.co.jp>
2018-06-18 17:46:01 +0900

    Initial Commit

---
+++ swift-calculator-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-calculator-app
+//
+//  Created by 中村麟太郎 on 2018/06/18.
+//  Copyright © 2018年 中村麟太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
